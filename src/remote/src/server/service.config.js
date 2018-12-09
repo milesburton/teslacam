fc18ddae75fbc250c1235a120e479360cb103014
miles <miles@mnetcs.com>
2018-12-09 17:09:53 +0000

    Hardening TeslaCam

--- src/remote/src/server/service.config.js
+++ src/remote/src/server/service.config.js
@@ -2,17 +2,17 @@ const services = [
     {
         label: 'Dashcam Service',
         type: 'service',
-        scriptCheckRunning: 'svok /root/teslacam/services/dashcam-monitor',
-        scriptStart: 'svc -u /root/teslacam/services/dashcam-monitor',
-        scriptStop: 'svc -d /root/teslacam/services/dashcam-monitor',
+        scriptCheckRunning: 'sudo /usr/bin/svok ~/teslacam/services/dashcam-monitor',
+        scriptStart: 'sudo /usr/bin/svc -u ~/teslacam/services/dashcam-monitor',
+        scriptStop: 'sudo /usr/bin/svc -d ~/teslacam/services/dashcam-monitor',
         state: false
     },
     {
         label: 'Dropbox Service',
         type: 'service',
-        scriptCheckRunning: 'svok /root/teslacam/services/dropbox-upload',
-        scriptStart: 'svc -u /root/teslacam/services/dropbox-upload',
-        scriptStop: 'svc -d /root/teslacam/services/dropbox-upload',
+        scriptCheckRunning: 'sudo /usr/bin/svok ~/teslacam/services/dropbox-upload',
+        scriptStart: 'sudo /usr/bin/svc -u ~/teslacam/services/dropbox-upload',
+        scriptStop: 'sudo /usr/bin/svc -d ~/teslacam/services/dropbox-upload',
         state: false
     },
 ];
