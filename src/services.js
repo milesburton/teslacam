94cfc72422bc27016fa57d5bd511a2d42d2edddf
miles <miles@mnetcs.com>
2018-12-09 15:37:26 +0000

    Added noddy service script to start/stop/status

---
+++ src/services.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+const fs = require('fs');
+const {execSync} = require('./common');
+
+const [path, script, command] = process.argv;
+
+console.log(command);
+
+
+const getServices = dirPath => fs
+    .readdirSync(dirPath, { withFileTypes: true})
+    .filter(f=>f.isDirectory());
+
+
+switch(command){
+
+    case 'start':
+        getServices('./services')
+            .forEach(s=>{
+                execSync(`svc -u ${s}`);
+            });
+        break;
+    case 'stop':
+        getServices('./services')
+            .forEach(s=>{
+                execSync(`svc -d ${s}`);
+            });
+        break;
+    case 'status':
+        getServices('./services')
+            .forEach(s=>{
+                execSync(`svstat ${s}`);
+            });
+        break;
+    default:
+        console.log('Unknown command. [start|stop]')
+
+}
