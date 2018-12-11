9a89e3588da154f06923f536b849572a2ad7c8da
miles <miles@mnetcs.com>
2018-12-11 07:44:10 +0000

    Reduce log verbosity

--- src/common.js
+++ src/common.js
@@ -1,35 +1,38 @@
-const { execSync: execSyncNoLogging } = require('child_process');
+const {execSync: execSyncNoLogging} = require('child_process');
 const {
-  performance: { now },
+    performance: {now},
 } = require('perf_hooks');
 
 const sleep = async ms => new Promise(r => setTimeout(r, ms));
 
-const execSync = (cmd, opts = { bubbleError: false }) => {
-  console.log(`Preparing to run command [${cmd}]`);
-  try {
-    const buffer = execSyncNoLogging(cmd);
+const execSync = (cmd, opts = {bubbleError: false}) => {
+    console.log(`Running [${cmd}]`);
+    try {
+        const buffer = execSyncNoLogging(cmd);
+        console.log('Process completed successfully');
+        if (buffer) {
+            console.log('======================= process');
+            console.log(buffer.toString());
+            console.log('======================= /process');
+        }
+    } catch (err) {
+        console.log('Process failed');
+        console.log('======================= error');
+        console.log(JSON.stringify(err));
+        console.log('======================= /error');
 
-    console.log('======================= process');
-    console.log(buffer.toString());
-    console.log('======================= /process');
-  } catch (err) {
-    console.log('======================= error');
-    console.log(JSON.stringify(err));
-    console.log('======================= /error');
-
-    if (opts.bubbleError) {
-      throw err;
+        if (opts.bubbleError) {
+            throw err;
+        }
     }
-  }
 };
 
 const benchmark = (fn) => {
-  const t0 = now();
-  fn();
-  const elapsedTimeMs = now() - t0;
-  console.log(`Took ${elapsedTimeMs} milliseconds`);
-  return elapsedTimeMs;
+    const t0 = now();
+    fn();
+    const elapsedTimeMs = now() - t0;
+    console.log(`Took ${elapsedTimeMs} milliseconds`);
+    return elapsedTimeMs;
 };
 
-module.exports = { sleep, execSync, benchmark };
+module.exports = {sleep, execSync, benchmark};
