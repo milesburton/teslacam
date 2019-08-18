95a6464a371074eb4dbfadc5612eed1d42c7ba42
Miles Burton <miles@mnetcs.com>
2019-08-18 16:08:02 +0100

    Added a few additional post install steps

--- installation/postinstall.sh
+++ installation/postinstall.sh
@@ -1,7 +1,15 @@
+
+# Update the pi to the latest dependencies
 apt update
 apt upgrade -y
-apt install -y daemontools daemontools-run
+
+# Install daemon tools to keep the scripts alive
+apt install -y daemontools daemontools-run git
+
+# Setup the teslacam folder and make sure everything is installed.
+chown -R pi:pi /home/pi/teslacam
 cd /home/pi/teslacam
+git pull #
 npm install
 cd src/remote
 npm install
