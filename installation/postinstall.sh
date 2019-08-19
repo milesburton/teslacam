438b8eb99bf668b6b0dacadffcbf3dafa03fcf1f
Miles Burton <miles@mnetcs.com>
2019-08-19 12:34:26 +0100

    Added some comments to postinstall

--- installation/postinstall.sh
+++ installation/postinstall.sh
@@ -1,4 +1,7 @@
 
+# The purpose of the post install is to clean up any final installation steps that could not be (easily) done when the disk image was being baked
+
+
 # Update the pi to the latest dependencies
 apt update
 apt upgrade -y
@@ -6,12 +9,19 @@ apt upgrade -y
 # Install daemon tools to keep the scripts alive
 apt install -y daemontools daemontools-run git
 
-# Setup the teslacam folder and make sure everything is installed.
-
+# Symlink node so it is on the $PATH
 ln -s /opt/node/bin/* /usr/bin
+
+# Setup the teslacam folder and make sure everything is installed.
 chown -R pi:pi /home/pi/teslacam
 cd /home/pi/teslacam
-git pull #
+
+# Make sure we're using the latest version of the teslacam code
+git pull
+
+# Install all the node dependencies
 npm install
 cd src/remote
 npm install
+
+echo "TeslaCam Ready!"
