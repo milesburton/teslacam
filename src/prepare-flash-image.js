df6f019c84dc6e64d5a1953d719fa05aa0f1acc8
Miles Burton <miles@mnetcs.com>
2019-08-20 18:08:13 +0100

    Corrected kernel params

--- src/prepare-flash-image.js
+++ src/prepare-flash-image.js
@@ -137,8 +137,8 @@ async function init() {
   execSyncOrFail(`touch ${mountPoint}/ssh`);
   console.log('Enabling USB OTG for fake thumb drive support');
   execSyncOrFail(`tr -d '\n' < ${mountPoint}/cmdline.txt && sed 's/rootwait//g ${mountPoint}/cmdline.txt`);
-  execSyncOrFail(`echo " loop.max_part=31 rootwait modules-load=dwc2,g_mass_storage " >> ${mountPoint}/cmdline.txt`);
-
+  execSyncOrFail(`echo " loop.max_part=31 rootwait modules-load=dwc2,g_mass_storage" >> ${mountPoint}/cmdline.txt`);
+  execSyncOrFail(`echo " dtoverlay=dwc2" >> ${mountPoint}/config.txt`);
 
   if (commandObject['--setup-wifi']) {
     console.log('Adding WIFI config');
